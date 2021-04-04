6ca40a9a37b06bd846cd7f47bb82af4f617d0258
Revo Tesha <revogatustesha@gmail.com>
2021-04-03 22:53:43 -0400

    change port

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-python manage.py runserver 0.0.0.0:8000 # this is the ip:port within the server.
+python manage.py runserver 0.0.0.0:$PATH # this is the ip:port within the server.
 # docker run -it --init -p 8000:8000  --name 'gan_app' image_id will connect the server port to
-# host port (also 8000).
+# host port (also 8000). Actually had to replace 8000 with $PATH here because heroku's changes
+# these dynamically. So you shouldn't hard code them when deploying.
