3b6cc3b49bfecc1601c5e8842684b21bc8bbe9df
Revo Tesha <revogatustesha@gmail.com>
2021-04-18 14:43:19 -0400

    added dummy model

--- stc_gan_app/views.py
+++ stc_gan_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import i2i_style_transfer
+
+model_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+model_output = i2i_style_transfer(model_input)
+
+in_out_data = {
+    {'type':'Model Input',
+     'data':model_input,
+     'meta_data':'Input image size:24MB',
+     'status':'Processing ... Pleae wait. Output image will display on the right.'},
+
+     'type':'Model Output',
+     'data': model_output,
+     'meta_data':'Processing time: 20s \n Output image size:24MB'
+     'status':'Done!'}
+     }
 
 # Create your views here.
 def app_home(request):
-    return render(request, 'stc_gan_app/app_home.html')
+    context = {
+        "in_out_data":in_out_data
+    }
+    return render(request, 'stc_gan_app/app_home.html', context)
