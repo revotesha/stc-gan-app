51b8e1cda59e5ba0c13ad8e88073c4355d7e5d1c
Revo Tesha <revogatustesha@gmail.com>
2021-04-18 14:48:05 -0400

    fixed minor error

--- stc_gan_app/views.py
+++ stc_gan_app/views.py
@@ -6,17 +6,18 @@ model_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 model_output = i2i_style_transfer(model_input)
 
-in_out_data = {
+in_out_data = [
+
     {'type':'Model Input',
      'data':model_input,
      'meta_data':'Input image size:24MB',
      'status':'Processing ... Pleae wait. Output image will display on the right.'},
 
-     'type':'Model Output',
+     {'type':'Model Output',
      'data': model_output,
-     'meta_data':'Processing time: 20s \n Output image size:24MB'
+     'meta_data':'Processing time: 20s \n Output image size:24MB',
      'status':'Done!'}
-     }
+]
 
 # Create your views here.
 def app_home(request):
